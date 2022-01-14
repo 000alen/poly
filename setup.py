eb6a3f12d8ce0818e90818847b07e53986ba5b11
Alen Rubilar <lclc.alen@gmail.com>
2022-01-14 01:39:25 -0300

    Lorem

--- setup.py
+++ setup.py
@@ -4,5 +4,7 @@ setuptools.setup(
     name="poly",
     version="0.0.0",
     packages=["poly"],
+    package_data={"": ["template.template"]},
+    include_package_data=True,
     python_requires=">=3.10",
 )
